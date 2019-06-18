ab1eefdc76a1b6d8e58d37658abff2230071c6e0
Radu <radu.focseneanu@gmail.com>
2019-06-18 14:11:16 +0300

    fix filtering on initial project list

--- src/projects/ProjectList.js
+++ src/projects/ProjectList.js
@@ -6,72 +6,77 @@ class ProjectList extends Component {
 
     constructor(){
         super();
+
+        let allProjects = [
+            {
+                "id": 1,
+                "code": "ONE",
+                "name": "Project ONE",
+                "description": "This is the description of Project ONE",
+                "owner_id": 10,
+                "owner_name": "James Hansen",
+                "created_date": "07/09/2051",
+                "end_date": "2051-10-19T21:00:00.000Z"
+            },
+            {
+                "id": 2,
+                "code": "TWO",
+                "name": "Project TWO",
+                "description": "This is the description of Project TWO",
+                "owner_id": 10,
+                "owner_name": "James Hansen",
+                "created_date": "24/10/2051",
+                "end_date": "2051-11-18T21:00:00.000Z"
+            },
+            {
+                "id": 3,
+                "code": "THREE",
+                "name": "Project THREE",
+                "description": "This is the description of Project THREE",
+                "owner_id": 10,
+                "owner_name": "James Hansen",
+                "created_date": "29/10/2051",
+                "end_date": "2051-11-30T21:00:00.000Z"
+            },
+            {
+                "id": 4,
+                "code": "FOUR",
+                "name": "Project FOUR",
+                "description": "This is the description of Project FOUR",
+                "owner_id": 11,
+                "owner_name": "Louise Meyer",
+                "created_date": "30/10/2051",
+                "end_date": "2051-12-07T21:00:00.000Z"
+            },
+            {
+                "id": 5,
+                "code": "FIVE",
+                "name": "Project FIVE",
+                "description": "This is the description of Project FIVE",
+                "owner_id": 11,
+                "owner_name": "Louise Meyer",
+                "created_date": "02/10/2051",
+                "end_date": "2051-11-15T21:00:00.000Z"
+            },
+            {
+                "id": 6,
+                "code": "SIX",
+                "name": "Project SIX",
+                "description": "This is the description of Project SIX",
+                "owner_id": 14,
+                "owner_name": "Clyde Dorman",
+                "created_date": "09/11/2051",
+                "end_date": "2051-12-22T21:00:00.000Z"
+            }
+        ]
+
         this.state = {
             currentProject: null,
-            projects: [
-                {
-                    "id": 1,
-                    "code": "ONE",
-                    "name": "Project ONE",
-                    "description": "This is the description of Project ONE",
-                    "owner_id": 10,
-                    "owner_name": "James Hansen",
-                    "created_date": "07/09/2051",
-                    "end_date": "2051-10-19T21:00:00.000Z"
-                },
-                {
-                    "id": 2,
-                    "code": "TWO",
-                    "name": "Project TWO",
-                    "description": "This is the description of Project TWO",
-                    "owner_id": 10,
-                    "owner_name": "James Hansen",
-                    "created_date": "24/10/2051",
-                    "end_date": "2051-11-18T21:00:00.000Z"
-                },
-                {
-                    "id": 3,
-                    "code": "THREE",
-                    "name": "Project THREE",
-                    "description": "This is the description of Project THREE",
-                    "owner_id": 10,
-                    "owner_name": "James Hansen",
-                    "created_date": "29/10/2051",
-                    "end_date": "2051-11-30T21:00:00.000Z"
-                },
-                {
-                    "id": 4,
-                    "code": "FOUR",
-                    "name": "Project FOUR",
-                    "description": "This is the description of Project FOUR",
-                    "owner_id": 11,
-                    "owner_name": "Louise Meyer",
-                    "created_date": "30/10/2051",
-                    "end_date": "2051-12-07T21:00:00.000Z"
-                },
-                {
-                    "id": 5,
-                    "code": "FIVE",
-                    "name": "Project FIVE",
-                    "description": "This is the description of Project FIVE",
-                    "owner_id": 11,
-                    "owner_name": "Louise Meyer",
-                    "created_date": "02/10/2051",
-                    "end_date": "2051-11-15T21:00:00.000Z"
-                },
-                {
-                    "id": 6,
-                    "code": "SIX",
-                    "name": "Project SIX",
-                    "description": "This is the description of Project SIX",
-                    "owner_id": 14,
-                    "owner_name": "Clyde Dorman",
-                    "created_date": "09/11/2051",
-                    "end_date": "2051-12-22T21:00:00.000Z"
-                }
-            ]
+            projects: allProjects,
+            allProjects: allProjects
         }
         this.setCurrentProject = this.setCurrentProject.bind(this);
+        this.onFilterChange = this.onFilterChange.bind(this)
     }
 
     setCurrentProject(project) {
@@ -80,7 +85,9 @@ class ProjectList extends Component {
 
     onFilterChange(searchText){
         console.log('Search Text: ', searchText)
+        let filteredProjects = this.state.allProjects.filter( (project) => project.name.toLowerCase().includes(searchText.toLowerCase()) )
 
+        this.setState(()=>({projects:filteredProjects}))
     }
 
     render() {
