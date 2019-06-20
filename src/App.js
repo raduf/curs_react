e056bd1216e6eadbd07c2d06c006c477af58e1b1
rdf <radu.focseneanu@gmail.com>
2019-06-20 11:11:15 +0300

    moved to next step (10_03 nested routes)

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import ProjectList from './projects/ProjectList';
 import { Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './layout/Navbar';
 import Dashboard from './dashboard/Dashboard';
 import Profile from './profile/Profile';
-import Navbar from './layout/Navbar';
 
 class App extends Component {
   title = 'Simple Project Manager';
@@ -17,9 +17,8 @@ class App extends Component {
   render() {
     return (
       <main className="container pt-3">
-        <div className="row justify-content-center">
-          <h1 className="col-12 bg-primary p-3 text-white"> <Navbar title={this.title} name={this.profile.name}/> </h1>
-        </div>
+          <Navbar title={this.title} />
+        
           <Route path="/" exact component={ProjectList} />
           <Route path="/dashboard" exact component={Dashboard} />
           <Route path="/profile" exact render={() => (<Profile profile={this.profile} />) } />
--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,38 +1,34 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 
 const Navbar = (props) => {
     return (
-        <nav className="navbar navbar-expand navbar-dark bg-primary text-white  justify-content-between">
-        <a className="navbar-brand">{props.title}</a>
-        
-        <ul className="navbar-nav">
-            <li className="nav-item active">
-                <a className="nav-link"></a>
-            </li>
-            <li className="nav-item">
-                <NavLink className="nav-link" to='/dashboard'>Dashboard</NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink 
-                    className="nav-link" 
-                    to='/' exact
-                    activeClassName='active'
-                    >Projects</NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink className="nav-link" to='/profile'>Profile</NavLink>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link disabled">{props.name}</a>
-            </li>
+        <nav className="navbar navbar-expand navbar-dark bg-primary text-white justify-content-between">
+            <a className="navbar-brand">{props.title}</a>
+
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <NavLink exact to='/dashboard' activeClassName="active"
+                        className="nav-link">Dashboard</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink exact to='/'  activeClassName="active"
+                        className="nav-link">Projects</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink exact to='/profile' activeClassName="active"
+                        className="nav-link">Profile</NavLink>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link disabled">James Hansen</a>
+                </li>
             </ul>
         </nav>
     )
 }
 
 Navbar.defaultProps = {
-    title: 'Simple PM'
+    title: 'SPM'
 }
 
 export default Navbar;
